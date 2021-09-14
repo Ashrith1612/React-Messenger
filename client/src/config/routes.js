aab7c727de3ce7cac34b3ebc6ca8b1759a5482e3
Ashrith1612 <ashrith.reddy@gmail.com>
2021-09-15 05:47:07 +0900

    Updated code structure

--- client/src/config/routes.js
+++ client/src/config/routes.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { fetchUser } from "./store/utils/thunkCreators";
-import Signup from "./Signup.js";
-import Login from "./Login.js";
-import { Home, SnackbarError } from "./components";
+import { fetchUser } from "../store/utils/thunkCreators";
+import Signup from "../pages/Signup.js";
+import Login from "../pages/Login.js";
+import { Home, SnackbarError } from "../components";
 
 const Routes = (props) => {
   const { user, fetchUser } = props;
