c1b39152575ed74498109af7195364132742e233
Ashrith1612 <ashrith.reddy@gmail.com>
2021-09-15 02:30:28 +0900

    Fix redux store issue

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -13,11 +13,9 @@ export const addMessageToStore = (state, payload) => {
   
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      var newConvo = Object.assign([], convo);
-      if (message.id > convo.messages.at(-1).id) {
-        newConvo.messages.push(message);
-        newConvo.latestMessageText = message.text;
-      }
+      let newConvo = Object.assign([], convo);
+      newConvo.messages.push(message);
+      newConvo.latestMessageText = message.text;
       return newConvo;
     } else {
       return convo;
@@ -72,10 +70,11 @@ export const addSearchedUsersToStore = (state, users) => {
 export const addNewConvoToStore = (state, recipientId, message) => {
   return state.map((convo) => {
     if (convo.otherUser.id === recipientId) {
-      convo.id = message.conversationId;
-      convo.messages.push(message);
-      convo.latestMessageText = message.text;
-      return convo;
+      let newConvo = Object.assign([], convo);
+      newConvo.id = message.conversationId;
+      newConvo.messages.push(message);
+      newConvo.latestMessageText = message.text;
+      return newConvo;
     } else {
       return convo;
     }
